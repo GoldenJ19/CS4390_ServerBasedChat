d63be59f7e1250a789e76b7c97fb3461c1912033
JHardy-23 <80800806+JHardy-23@users.noreply.github.com>
2022-03-11 20:47:39 -0600

    Starting Files
    Added starting files that we can build off.

---
+++ Client/Client.java
@@ -0,0 +1,11 @@
+/*
+ *  File Name:      Client.java
+ *  Author:         Justin Hardy
+ *  Last Modified:  03/11/2022
+ */
+
+public class Client {
+    public static void main(String[] args) {
+        System.out.println("Hello World!");
+    }
+}
---
+++ Server/Server.java
@@ -0,0 +1,11 @@
+/*
+ *  File Name:      Server.java
+ *  Author:         Justin Hardy
+ *  Last Modified:  03/11/2022
+ */
+
+public class Server {
+    public static void main(String[] args) {
+        System.out.println("Hello World!");
+    }
+}
