4e5e697c325b9dacffc4876437ff6b50ca8f1014
Ahad Shabbir <ahad.shabbir@mailbox.org>
2022-03-13 13:02:23 -0500

    updated server file

--- Server/Server.java
+++ Server/Server.java
@@ -1,11 +1,36 @@
-/*
- *  File Name:      Server.java
- *  Author:         Justin Hardy
- *  Last Modified:  03/11/2022
- */
+package javatcpexample;
 
-public class Server {
-    public static void main(String[] args) {
-        System.out.println("Hello World!");
+import java.net.Socket;
+import java.net.ServerSocket;
+import java.io.PrintWriter;
+import java.io.BufferedReader; 
+import java.io.InputStreamReader;
+
+public class GreetServer {
+    private ServerSocket serverSocket;
+    private Socket clientSocket;
+    private PrintWriter out;
+    private BufferedReader in;
+
+    public void start(int port) throws Exception {
+        serverSocket = new ServerSocket(port);
+        clientSocket = serverSocket.accept();
+        out = new PrintWriter(clientSocket.getOutputStream(), true);
+        in = new BufferedReader(new InputStreamReader(clientSocket.getInputStream()));
+
+        String clientMsg = in.readLine();
+		System.out.println("got client message: " + clientMsg);	
+		out.printf("got this message from you '%s'\n", clientMsg);
+    }
+
+    public void stop() throws Exception {
+        in.close();
+        out.close();
+        clientSocket.close();
+        serverSocket.close();
+    }
+    public static void main(String[] args) throws Exception {
+        GreetServer server = new GreetServer();
+        server.start(6666);
     }
 }
