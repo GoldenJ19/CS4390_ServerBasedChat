79dffb2d1c28b13b073bbc5539aa37c58f203c4d
Ahad Shabbir <ahad.shabbir@mailbox.org>
2022-04-04 01:32:22 -0500

    initial changes

--- src/main/java/org/teamnine/server/ConnctionHandler.java
+++ src/main/java/org/teamnine/server/ConnctionHandler.java
@@ -6,18 +6,21 @@ import java.util.Scanner;
 
 import org.teamnine.common.ParseBuilder;
 
-public class ConnectionHandler implements Callable<Void> {
-	private Server parent;
+public class ConnectionHandler implements Runnable {
+	private ChatRoom chatRoom;
 	private Socket clientSocket;
 	private ParseBuilder pb;	
 	private ConnectionHandler clientb;
+	private String username;
 
-	public ConnectionHandler(Server parent, Socket clientSocket) {
-		this.parent = parent;
+	public ConnectionHandler(ChatRoom chatRoom, Socket clientSocket) {
+		this.chatRoom = chatRoom;
 		this.clientSocket = clientSocket;
+		// Read client socket for connect request - put user in username
 	}
 	
-	public Void call() {
+	// Changed to run - handle all exceptions, and send responses as necessary.
+	public void run() {
 		while (true) {
 			// Read from socket
 			// Validate calls and parse msgtype
@@ -25,6 +28,8 @@ public class ConnectionHandler implements Callable<Void> {
 		}
 	} 
 
+	public String getUsername() { return username; }
 	private void onChatRequest() {}
 	private void onEndSession() {}
+
 }
--- src/main/java/org/teamnine/server/Server.java
+++ src/main/java/org/teamnine/server/Server.java
@@ -5,97 +5,44 @@ import java.net.ServerSocket;
 import java.net.Socket;
 import java.util.Scanner;
 
-import org.teamnine.common.ParseBuilder;
-
 public class Server {
 	private ServerSocket serverSocket;
-	//private Socket clientSocket;
-	//private PrintWriter out;
-	//private Scanner in;
-	private ParseBuilder pb;	
-
+	private ChatRoom chatRoom;
 	private String[] subbedUsers = {"AHAD", "GRANT"};
-	
-	private Map<String, ConnectionHandler> connectedUsers;
-	private Map<String, ConnectionHandler> busyUsers;
+	private List<Thread> threads;
 
-	public void start(int port) throws Exception {
+	public Server(int port) {
 		serverSocket = new ServerSocket(port);
-		
-		while (true) {
-			clientSocket = serverSocket.accept();
-			out = new PrintWriter(clientSocket.getOutputStream(), true);
-			in = new Scanner(clientSocket.getInputStream());
-			pb = new ParseBuilder(in);
+		chatRoom = new ChatRoom();
+		threads = new ArrayList<Thread>();
+	}
 
-			String msgType;
-			msgType = pb.pass("START").pass("MSGTYPE:").extract();
-			switch(msgType) {
-				case "CONNECT":
-					connectHandler();
-					break;
-				default:
-					throw new Exception("invalid msgType");
-			}
+	public void start() throws Exception {
+		while (true) {
+			Socket clientSocket = serverSocket.accept();
+			ConnectionHandler ch = new ConnectionHandler(clientSocket, chatRoom);
+			chatRoom.registerUser(ch);	
+			threads.add(new Thread(ch));
 		}
 	}
 
-	public boolean isUserValid(String username) {}
-	public ConnectionHandler getUserHandler(String username) {}
-		
 	public void stop() throws Exception {
-		if (in != null)
-			in.close();
-		
-		if (out != null)
-			out.close();
-		
-		if (clientSocket != null)
-			clientSocket.close();
-		
 		if (serverSocket != null)
 			serverSocket.close();
-	}
-	
-	private void connectHandler() throws Exception {
-		ParseBuilder pb = new ParseBuilder(in);
-		String username = pb.pass("USERNAME:").extract();
-		String randCookieStr = pb.pass("RAND_COOKIE:").extract();
 
-		int rand_cookie = Integer.parseInt(randCookieStr);
-		System.out.println("rand_cookie = " + rand_cookie);
+		for (Thread thread : threads)
+			thread.interrupt();
 
-		boolean isSubbed = false;
-		for (String user : subbedUsers) {
-			if (username.equals(user)) {
-				isSubbed = true;
-				break;
-			}
-		}
-		
-		if (isSubbed) {
-			System.out.println("User " + username + " is valid.");
-			// Create ConnectionHandler for thread
-			connectedResponse();
-			// Add ConnectionHandler user key pait to hashmap
-		} else {
-			System.out.println("User " + username + " is NOT valid.");
-			stop();
-		}
-	}
-
-	private void connectedResponse() {
-		out.printf("START\nMSGTYPE: CONNECTED\nEND\n");
+		for (Thread thread : threads) 
+			thread.join();
 	}
 
 	public static void main(String[] args) throws Exception {
-		Server server = new Server();
+		Server server = new Server(6666);
 		try {
-			server.start(6666);
+			server.start();
 		} finally {
 			server.stop();
 		}
 	}
-	
-	
 }
