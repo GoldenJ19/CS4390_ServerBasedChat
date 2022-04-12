4ccda7e09eca6bf88d1602d52d83e711d02f60fd
Ahad Shabbir <ahad.shabbir@mailbox.org>
2022-04-11 21:50:07 -0500

    skeleton of server design

---
+++ src/main/java/org/teamnine/server/Authenticator/Authenticator.java
@@ -0,0 +1,11 @@
+package org.teamnine.Server.Authenticator;
+
+public class Authenticator implements Runnable {
+	
+	public Authenticator(int port) {}
+	// public Authenticator(int port, String dbaddr)
+	
+	public void run() {}
+	
+	public boolean validateRandCookie(int randCookie) { return true; }
+}
