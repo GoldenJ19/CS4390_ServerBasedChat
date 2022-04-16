5782f95e53a70fc10a87de8533db35a43aa3d721
Derp1ing <79543032+Derp1ing@users.noreply.github.com>
2022-04-16 14:09:00 -0500

    Update Authenticator.java

--- src/main/java/org/teamnine/server/Authenticator/Authenticator.java
+++ src/main/java/org/teamnine/server/Authenticator/Authenticator.java
@@ -3,17 +3,27 @@ import java.math.BigInteger;
 import java.security.MessageDigest;
 import java.security.NoSuchAlgorithmException;
 import java.net.DatagramSocket;
+import java.net.SocketException;
 
 public class Authenticator implements Runnable {
+	private int[] K_Alist;
 	private int[] randCookieList;
-	public Authenticator(int port) {
-		
+	private DatagramSocket clientSocket;
+	private int rand;
+	
+	public Authenticator(int port) throws SocketException {
+		clientSocket = new DatagramSocket(port);
+		rand = (int) Math.floor(Math.random()*(9999-1000+1)+1000);
 	}
 	// public Authenticator(int port, String dbaddr)
 	
-	public void run() {}
+	public void run() {
+		//Connect database to K_Alist
+		
+	}
 	
-
+	//Returns T/F depending on if a randCookie is in the list of known
+	//randCookies
 	public boolean validateRandCookie(int randCookie) { 
 		for(int i : randCookieList) {
 			if(randCookie == i)
@@ -24,8 +34,12 @@ public class Authenticator implements Runnable {
 		return false;
 		}
 	
-	public String hashMD5(String input) {
+	//Returns a MD5 hashed string, where the input string is 
+	//a randCookie and client K_A concatenated
+	public String A3(int rand, int K_A) {
 		try {  
+			String input = String.valueOf(rand) + String.valueOf(K_A);
+			
             // Static getInstance method is called with hashing MD5
             MessageDigest md = MessageDigest.getInstance("MD5");
   
@@ -43,16 +57,50 @@ public class Authenticator implements Runnable {
             }
             return hashtext;
         } 
-  
+	
         // For specifying wrong message digest algorithms
         catch (NoSuchAlgorithmException e) {
             throw new RuntimeException(e);
         }	
 	}
 	
-	public boolean matching(String orig, String compare){
-	String newComp = hashMD5(compare);
+	
+	//Returns a SHA1 hashed string, where the input string is 
+	//a randCookie and client K_A concatenated
+	public String A8(int rand, int K_A) {
+		try {
+			String input = String.valueOf(rand) + String.valueOf(K_A);
+			
+			MessageDigest digest = MessageDigest.getInstance("SHA-1");
+	        digest.reset();
+	        digest.update(input.getBytes());
+	        String hashtext = String.format("%040x", new BigInteger(1, digest.digest()));
+	        
+	        return hashtext;
+		} 
+		
+		catch (NoSuchAlgorithmException e) {
+            throw new RuntimeException(e);
+        }	
+		
+	}
+	
+	
+	//Boolean function that compares a MD5 hash to some randCookie and K_A
+	public boolean A3match(String orig, int rand, int K_A){
+	String newComp = A3(rand, K_A);
 	if(orig.equals(newComp)) {return true;}
 	else {return false;}
+	}
+	
+	//Boolean function that compares a SHA1 hash to some randCookie and K_A
+	public boolean A8match(String orig, int rand, int K_A){
+		String newComp = A8(rand, K_A);
+		if(orig.equals(newComp)) {return true;}
+		else {return false;}
+		}
+	
+	public int getRandCookie() {
+		return rand;
 	}
 }
